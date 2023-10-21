b184e8b0568fed795a97501abdaf29fc51121de7
jubaer123 <jubaerhafizurrahman6@gmail.com>
2023-10-21 04:30:23 -0300

    Front_Layout_Part1

--- app/src/main/java/ca/unb/mobiledev/weatherapp/SplashActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/weatherapp/SplashActivity.kt
@@ -14,6 +14,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        },30000)
     }
 }
